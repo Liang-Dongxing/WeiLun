1fb95f686a0985947a829326f3c3004eacc74c96
LiangYong <liangdongxing.cn@outlook.com>
2019-12-09 22:48:54 +0800

    添加log测试Module

---
+++ SpringBoot/log/src/test/java/com/weilun/log/LogTest.java
@@ -0,0 +1,12 @@
+package com.weilun.log;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author DongXing
+ * @user Liang
+ * @time 2019/12/9 22:45
+ */
+@SpringBootTest
+public class LogTest {
+}
